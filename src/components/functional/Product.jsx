f4b4c01178e55cfb1baba43b97a930bd359ed620
Thuzar <thuzar.ts92@gmail.com>
2024-11-06 13:11:27 +0700

    Read more btn updated

--- src/components/functional/Product.jsx
+++ src/components/functional/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({ item, type }) => {
       {item && (
         <div
           key={item.id}
-          className="rounded-lg bg-gray-100 p-3 shadow duration-150 hover:scale-105 hover:shadow-md h-full"
+          className="relative rounded-lg bg-gray-100 p-3 shadow duration-150 hover:scale-105 hover:shadow-md h-full"
         >
           <img
             className="w-full rounded-lg object-cover object-center"
@@ -16,9 +16,27 @@ const Product = ({ item, type }) => {
           {type == 'category' && (
             <a
               href="/products"
-              class="tag-line-font text-xs tracking-wider hover:bg-primary-700 font-bold inline-block bg-primary-500 text-white rounded px-3 py-2"
+              class="absolute -bottom-3 -right-3 tag-line-font text-xs tracking-wider hover:bg-primary-700 font-bold inline-block bg-primary-500 text-white rounded-md px-3 py-2"
             >
-              See More
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"
+              >
+                <>
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M5 12l14 0" />
+                  <path d="M15 16l4 -4" />
+                  <path d="M15 8l4 4" />
+                </>
+              </svg>
             </a>
           )}
         </div>
